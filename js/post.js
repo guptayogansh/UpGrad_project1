a45e1c5c289bbf1b5533be5c9cd94e5f2a23826f
guptayogansh <yogansh.11@gmail.com>
2019-08-09 13:51:04 +0530

    Part C completed, part B remaining

--- js/post.js
+++ js/post.js
@@ -1,17 +1,60 @@
 var edit = document.getElementById("edit");
 var save = document.getElementById("save");
+var blogBody = document.getElementById("blogBody");
 edit.onclick = function(){
   edit.style.display="none";
   save.style.display="block";
-  document.getElementById("blogBody").style.boxShadow = "inset 0 0 5px #000";
-  document.getElementById("blogBody").contentEditable="true";
+  blogBody.style.boxShadow = "inset 0 0 5px #000";
+  blogBody.contentEditable="true";
 }
 
 save.onclick = function(){
   edit.style.display="block";
   save.style.display="none";
-  document.getElementById("blogBody").style.boxShadow = "none";
-  document.getElementById("blogBody").contentEditable="false";
+  blogBody.style.boxShadow = "none";
+  blogBody.contentEditable="false";
 
 }
 
+var likebtn = document.getElementById("like");
+var count=0;
+likebtn.onclick = function(){
+  if(likebtn.innerHTML == "Like")
+    {
+     likebtn.innerHTML = "Liked!";
+     count++;
+     document.getElementById("counter").innerText = count + "  person likes this!";
+
+    }
+  else
+  {
+    likebtn.innerHTML = "Like";
+    count --;
+    if(count==0){
+      document.getElementById("counter").innerText = "Be the first person to like this!";
+    }
+    else{
+      document.getElementById("counter").innerText = count + "  person likes this!";
+
+    }
+  }
+}
+
+
+var commentBtn = document.getElementById("commentBtn");
+commentBtn.onclick = function(){
+  text = commentArea.value;
+  var p = document.createElement("p");
+   p.style.background="white";
+   p.style.margin="10px";
+   p.style.padding="10px";
+   p.style.wordWrap = "break-word";
+  var node = document.createTextNode(text);
+  p.prepend(node);
+  var element = document.getElementById("commentContainer");
+  element.prepend(p);
+  var textArea = document.getElementById("commentArea");
+  if(!textArea.value || textArea.value!=textArea.defaultValue){
+   textArea.value = textArea.defaultValue;
+  }
+}
